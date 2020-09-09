8520ac90682658fc9283fc83c1cf9cc7224021aa
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2020-09-09 13:55:10 +0300

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,21 @@
+def encryption(fock):
+    encrypt = fock
+    key = 3
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alphabet += alphabet.lower()
+    encrypted = ""
+    for letter in encrypt:
+        position = alphabet.find(letter)
+        newPosition = position + key
+        if letter in alphabet:
+            encrypted = encrypted + alphabet[newPosition]
+        else:
+            encrypted = encrypted + letter
+    return encrypted
+
+f1 = open('encrypt.txt', 'r')
+text = "".join(f1)
+gg = encryption(text)
+f2 = open('encrypted.txt', 'w')
+f2.writelines(gg)
+f2.close()
